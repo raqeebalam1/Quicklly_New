4f185946cdc2335e4e2934edafca554f41bbe41f
raqeebalam1 <97635811+raqeebalam1@users.noreply.github.com>
2023-02-03 04:32:17 +0500

    Update testing_ReadyToEat.py

--- tests/Departments/ReadyToEat/testing_ReadyToEat.py
+++ tests/Departments/ReadyToEat/testing_ReadyToEat.py
@@ -13,18 +13,18 @@ class TesROTIKIT(ui_test_class.UVXVXIIIClass):
     actual2 = " Order Roti Kit"
     actual3 = "Search for products..."
     actual4 = "Thank you"
-    actual5 = "Butter Chicken with Basmati Rice and side of Naan  "
-    actual6 = "Chicken Madras with Lemon Rice and side of Naan  "
-    actual7 = "Chicken Vindaloo with Basmati Rice and side of Naan  "
-    actual8 = "Yellow Lentil with Basmati Rice and side of Naan  "
-    actual9 = "Chana Masala with Lemon Rice and side of Naan  "
-    actual10= "Tofu Tikka Masala with Basmati Rice and side of Naan  "
-    actual11 = "Paneer Tikka Masala with Basmati Rice and side of Naan  "
-    actual12 = "Paneer Cashew Korma with Basmati Rice and side of Naan  "
+    actual5 = "Butter Chicken with Basmati Rice  "
+    actual6 = "Chicken Madras with Lemon Rice  "
+    actual7 = "Chicken Vindaloo with Basmati Rice  "
+    actual8 = "Yellow Lentil with Basmati Rice  "
+    actual9 = "Chana Masala with Lemon Rice  "
+    actual10= "Tofu Tikka Masala with Basmati Rice  "
+    actual11 = "Paneer Tikka Masala with Basmati Rice  "
+    actual12 = "Paneer Cashew Korma with Basmati Rice  "
     actual13 = "Veg Biryani  "
     actual14 = "Chicken Biryani  "
-    actual15 = "Chicken Tikka Masala with Basmati Rice and side of Naan  "
-    actual16 = "Dal Makhani Kit + 2 Rotis  "
+    actual15 = "Chicken Tikka Masala with Basmati Rice  "
+    actual16 = "Dudhi Halwa Kit  "
     actual17 = "Dal Tadka Kit + 2 Rotis  "
     actual18 = "Sambhar Kit + 2 Rotis  "
     actual19 = "Chicken Korma + 2 Rotis  "
@@ -45,16 +45,16 @@ class TesROTIKIT(ui_test_class.UVXVXIIIClass):
         cls.driver.quit()
 
 
-    def test_Signin(self):
-        self.roti_page.select_dropdown()
-        self.roti_page.click_signin()
-        self.roti_page.EnterEmail("testaccount@quicklly.com")
-        self.roti_page.EnterPass("123456")
-        self.roti_page.click_login()
-        time.sleep(2)
-        alert = self.driver.switch_to.alert
-        if alert:
-            alert.accept()
+    # def test_Signin(self):
+    #     # self.roti_page.select_dropdown()
+    #     # self.roti_page.click_signin()
+    #     self.roti_page.EnterEmail("testaccount@quicklly.com")
+    #     self.roti_page.EnterPass("123456")
+    #     self.roti_page.click_login()
+    #     time.sleep(2)
+        # alert = self.driver.switch_to.alert
+        # if alert:
+        #     alert.accept()
 
     def test_EnterZipCode(self):
         self.roti_page.zip("60611")
@@ -74,9 +74,9 @@ class TesROTIKIT(ui_test_class.UVXVXIIIClass):
         time.sleep(5)
         self.roti_page.click_ButterChicken()
         self.roti_page.click_PlusButterChicken()
+        self.roti_page.click_PlusButterChicken()
         self.roti_page.click_ChickenMadras()
         self.roti_page.click_ChickenVindaloo()
-        self.roti_page.click_ChickenBiryani()
         self.roti_page.click_DetailsChickenMadras()
         self.roti_page.click_CloseDetailsChickenMadras()
         self.roti_page.click_Addtocart()
@@ -150,102 +150,102 @@ class TesROTIKIT(ui_test_class.UVXVXIIIClass):
         self.assertEqual(self.actual14, label)
         time.sleep(10)
 
-    def test_labelReadyToEat11(self):
-        time.sleep(10)
-        label = self.roti_page.get_attribute(RotiKit.labelReadytoeat11, 'textContent')
-        print(label)
-        self.assertEqual(self.actual15, label)
-        time.sleep(10)
+    # def test_labelReadyToEat11(self):
+    #     time.sleep(10)
+    #     label = self.roti_page.get_attribute(RotiKit.labelReadytoeat11, 'textContent')
+    #     print(label)
+    #     self.assertEqual(self.actual15, label)
+    #     time.sleep(10)
+    #
+    # def test_labelReadyToEat12(self):
+    #     label = self.roti_page.get_attribute(RotiKit.labelReadytoeat12, 'textContent')
+    #     print(label)
+    #     self.assertEqual(self.actual16, label)
+    #     time.sleep(10)
+    #
+    # def test_labelReadyToEat13(self):
+    #     time.sleep(10)
+    #     label = self.roti_page.get_attribute(RotiKit.labelReadytoeat13, 'textContent')
+    #     print(label)
+    #     self.assertEqual(self.actual17, label)
+    #     time.sleep(10)
+    #
+    # def test_labelReadyToEat14(self):
+    #     label = self.roti_page.get_attribute(RotiKit.labelReadytoeat14, 'textContent')
+    #     print(label)
+    #     self.assertEqual(self.actual18, label)
+    #     time.sleep(10)
+    #
+    # def test_labelReadyToEat15(self):
+    #     time.sleep(10)
+    #     label = self.roti_page.get_attribute(RotiKit.labelReadytoeat15, 'textContent')
+    #     print(label)
+    #     self.assertEqual(self.actual19, label)
+    #     time.sleep(10)
+    #
+    # def test_labelReadyToEat16(self):
+    #     label = self.roti_page.get_attribute(RotiKit.labelReadytoeat16, 'textContent')
+    #     print(label)
+    #     self.assertEqual(self.actual20, label)
+    #     time.sleep(10)
+    #
+    # def test_labelReadyToEat17(self):
+    #     label = self.roti_page.get_attribute(RotiKit.labelReadytoeat17, 'textContent')
+    #     print(label)
+    #     self.assertEqual(self.actual20, label)
+    #     time.sleep(10)
+    #
+    # def test_labelReadyToEat18(self):
+    #     label = self.roti_page.get_attribute(RotiKit.labelReadytoeat18, 'textContent')
+    #     print(label)
+    #     self.assertEqual(self.actual20, label)
+    #     time.sleep(10)
+    #
+    # def test_labelReadyToEat19(self):
+    #     label = self.roti_page.get_attribute(RotiKit.labelReadytoeat19, 'textContent')
+    #     print(label)
+    #     self.assertEqual(self.actual20, label)
+    #     time.sleep(10)
+    #
+    # def test_labelReadyToEat20(self):
+    #     label = self.roti_page.get_attribute(RotiKit.labelReadytoeat20, 'textContent')
+    #     print(label)
+    #     self.assertEqual(self.actual20, label)
+    #     time.sleep(10)
+    #
+    # def test_labelReadyToEat21(self):
+    #     label = self.roti_page.get_attribute(RotiKit.labelReadytoeat21, 'textContent')
+    #     print(label)
+    #     self.assertEqual(self.actual20, label)
+    #     time.sleep(10)
+    #
+    # def test_labelReadyToEat22(self):
+    #     label = self.roti_page.get_attribute(RotiKit.labelReadytoeat22, 'textContent')
+    #     print(label)
+    #     self.assertEqual(self.actual20, label)
+    #     time.sleep(10)
 
-    def test_labelReadyToEat12(self):
-        label = self.roti_page.get_attribute(RotiKit.labelReadytoeat12, 'textContent')
-        print(label)
-        self.assertEqual(self.actual16, label)
-        time.sleep(10)
-
-    def test_labelReadyToEat13(self):
-        time.sleep(10)
-        label = self.roti_page.get_attribute(RotiKit.labelReadytoeat13, 'textContent')
-        print(label)
-        self.assertEqual(self.actual17, label)
-        time.sleep(10)
-
-    def test_labelReadyToEat14(self):
-        label = self.roti_page.get_attribute(RotiKit.labelReadytoeat14, 'textContent')
-        print(label)
-        self.assertEqual(self.actual18, label)
-        time.sleep(10)
-
-    def test_labelReadyToEat15(self):
-        time.sleep(10)
-        label = self.roti_page.get_attribute(RotiKit.labelReadytoeat15, 'textContent')
-        print(label)
-        self.assertEqual(self.actual19, label)
-        time.sleep(10)
-
-    def test_labelReadyToEat16(self):
-        label = self.roti_page.get_attribute(RotiKit.labelReadytoeat16, 'textContent')
-        print(label)
-        self.assertEqual(self.actual20, label)
-        time.sleep(10)
-
-    def test_labelReadyToEat17(self):
-        label = self.roti_page.get_attribute(RotiKit.labelReadytoeat17, 'textContent')
-        print(label)
-        self.assertEqual(self.actual20, label)
-        time.sleep(10)
-
-    def test_labelReadyToEat18(self):
-        label = self.roti_page.get_attribute(RotiKit.labelReadytoeat18, 'textContent')
-        print(label)
-        self.assertEqual(self.actual20, label)
-        time.sleep(10)
-
-    def test_labelReadyToEat19(self):
-        label = self.roti_page.get_attribute(RotiKit.labelReadytoeat19, 'textContent')
-        print(label)
-        self.assertEqual(self.actual20, label)
-        time.sleep(10)
-
-    def test_labelReadyToEat20(self):
-        label = self.roti_page.get_attribute(RotiKit.labelReadytoeat20, 'textContent')
-        print(label)
-        self.assertEqual(self.actual20, label)
-        time.sleep(10)
-
-    def test_labelReadyToEat21(self):
-        label = self.roti_page.get_attribute(RotiKit.labelReadytoeat21, 'textContent')
-        print(label)
-        self.assertEqual(self.actual20, label)
-        time.sleep(10)
-
-    def test_labelReadyToEat22(self):
-        label = self.roti_page.get_attribute(RotiKit.labelReadytoeat22, 'textContent')
-        print(label)
-        self.assertEqual(self.actual20, label)
-        time.sleep(10)
-
-    def test_labelReadyToEat23(self):
-        label = self.roti_page.get_attribute(RotiKit.labelReadytoeat23, 'textContent')
-        print(label)
-        self.assertEqual(self.actual20, label)
-        time.sleep(10)
-
-    def test_labelReadyToEat24(self):
-        label = self.roti_page.get_attribute(RotiKit.labelReadytoeat24, 'textContent')
-        print(label)
-        self.assertEqual(self.actual20, label)
-        time.sleep(10)
-
-    def test_labelReadyToEat25(self):
-        label = self.roti_page.get_attribute(RotiKit.labelReadytoeat25, 'textContent')
-        print(label)
-        self.assertEqual(self.actual20, label)
-        time.sleep(10)
-
-    def test_labelReadyToEat26(self):
-        label = self.roti_page.get_attribute(RotiKit.labelReadytoeat26, 'textContent')
-        print(label)
-        self.assertEqual(self.actual20, label)
-        time.sleep(10)
+    # def test_labelReadyToEat23(self):
+    #     label = self.roti_page.get_attribute(RotiKit.labelReadytoeat23, 'textContent')
+    #     print(label)
+    #     self.assertEqual(self.actual20, label)
+    #     time.sleep(10)
+    #
+    # def test_labelReadyToEat24(self):
+    #     label = self.roti_page.get_attribute(RotiKit.labelReadytoeat24, 'textContent')
+    #     print(label)
+    #     self.assertEqual(self.actual20, label)
+    #     time.sleep(10)
+    #
+    # def test_labelReadyToEat25(self):
+    #     label = self.roti_page.get_attribute(RotiKit.labelReadytoeat25, 'textContent')
+    #     print(label)
+    #     self.assertEqual(self.actual20, label)
+    #     time.sleep(10)
+    #
+    # def test_labelReadyToEat26(self):
+    #     label = self.roti_page.get_attribute(RotiKit.labelReadytoeat26, 'textContent')
+    #     print(label)
+    #     self.assertEqual(self.actual20, label)
+    #     time.sleep(10)
 
